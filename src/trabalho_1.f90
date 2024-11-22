55149e45e4312cdf2145f8d024903bd32fe1960c
igor-inoue <igor.inoue@ufv.br>
2024-11-22 16:49:45 -0300

    cria repositório

---
+++ src/trabalho_1.f90
@@ -0,0 +1,10 @@
+module trabalho_1
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, trabalho_1!"
+  end subroutine say_hello
+end module trabalho_1
