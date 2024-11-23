f13b674e2993a0029c16ddeab40299014aad1a21
igor-inoue <igor.inoue@ufv.br>
2024-11-23 00:52:39 -0300

    Cria programa para calcular integral

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,15 @@
 program main
-  use trabalho_1, only: say_hello
+  use m_integral
+  use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
   implicit none
+  real(kind=sp) :: a, b, r
 
-  call say_hello()
+  a = 1.0
+  b = 3.0
+
+  r = integral(a,b)
+
+  write(*,'(a, f4.2, a, f4.2)') "A integral do seno de x variando de ", a, " até ", b, " é:"
+  write(*,*) r
+
 end program main
