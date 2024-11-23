8a96621c1d7da3518f289a4f74f701ffa61c42cf
igor-inoue <igor.inoue@ufv.br>
2024-11-23 01:31:26 -0300

    Permite inserir o número de partições

--- app/main.f90
+++ app/main.f90
@@ -2,14 +2,18 @@ program main
   use m_integral
   use m_limites
   use mod_tictoc
+  use m_n
   use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
   implicit none
 
   real(kind=sp) :: a, b, r
+  integer(kind=i4) :: N
 
   type(tictoc) :: cronometro
 
   call limites(a,b)
+
+  call sN(N)
 
   call cronometro%reset()
   call cronometro%tic()
@@ -21,7 +25,7 @@ program main
   write(*,'(a, f4.2, a, f4.2)') "A integral do seno de x variando de ", a, " até ", b, " é:"
   write(*,*) r
 
-  write(*,*) "O tempo utilizado para calculá-la utilizando 100 partições", "foi:"
+  write(*,*) "O tempo utilizado para calculá-la utilizando ", N, " partições", "foi:"
   write(*,*) cronometro%t_tot, " segundos"
 
 end program main
